d38113342d9841aeb7256fdd43d62646729b4d89
Will Moran <will.c.moran@gmail.com>
2024-05-08 11:39:41 -0400

    Added function and event listener that handles changes to size and reflects in changes to transientState

--- scripts/sizeOptions.js
+++ scripts/sizeOptions.js
@@ -1,8 +1,23 @@
+import { setSize } from "./transientState.js";
+
+export const handleSizeChange = (changeEvent) => {
+    // Check that what was changed is the size
+    if (changeEvent.target.name === 'size') {
+        // Grab the id from the state of what was clicked and parseInt
+        const convertedToInteger = parseInt(changeEvent.target.value)
+        // Invoke function defined in the transientState module to change the sizeId of the transient state
+        setSize(convertedToInteger)
+    }
+}
+
 export const sizeOptions = async () => {
     // Get size info from api
     const response = await fetch("http://localhost:8088/sizes");
     // Convert info to JS object and store in sizes variable
     const sizes = await response.json();
+
+    // Define an event listener that listens for size to be changed
+    document.addEventListener("change", handleSizeChange)
 
     // Declare a variable to begin building html info
     let sizeHTML = `<section class='card'><h2 class='head'>Sizes</h2>`;
