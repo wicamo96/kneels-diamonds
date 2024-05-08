113c034b590f3fd01209d092ea8ea9d01d723d2a
Will Moran <will.c.moran@gmail.com>
2024-05-08 13:40:17 -0400

    Modified fetch statement to return more info about each order to calculate and display price of each order

--- scripts/orders.js
+++ scripts/orders.js
@@ -1,12 +1,14 @@
 export const Orders = async () => {
-    const fetchResponse = await fetch("http://localhost:8088/orders")
+    const fetchResponse = await fetch("http://localhost:8088/orders?_expand=metal&_expand=style&_expand=size")
     const orders = await fetchResponse.json()
 
     let ordersHTML = ``
 
     let temp = orders.map((order) => {
+            const orderPrice = order.metal.price + order.style.price + order.size.price
+
            return `<div class='orderEntry'>
-            <strong>Order #${order.id}</strong>
+            <strong>Order #${order.id} costs $${orderPrice}</strong>
            </div>`
         }
     )
